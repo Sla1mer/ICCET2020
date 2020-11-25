d39cfcaa67e4ec688fc2376f31da7a8bec8b2b1b
frishart500 <63844334+frishart500@users.noreply.github.com>
2020-11-25 20:25:08 +0200

    много корректив и +RegistritActivity и TakeNoteActivity

--- app/src/main/java/com/example/iccet2020/LoginActivity.java
+++ app/src/main/java/com/example/iccet2020/LoginActivity.java
@@ -1,29 +1,44 @@
 package com.example.iccet2020;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.datepicker.MaterialTextInputPicker;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText email;
     private TextInputEditText password;
-    private MaterialButton signinBtn;
+    private Button signinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+
+       signinBtn.setOnClickListener(BTNs);
     }
+    View.OnClickListener BTNs = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.registration){
+                Intent intent = new Intent(getApplicationContext(), TakeNoteActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 
     // Инициализация элементов интерфейса
     private void init()
     {
-        signinBtn = findViewById(R.id.sign_in);
+        signinBtn = findViewById(R.id.registration);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
     }
