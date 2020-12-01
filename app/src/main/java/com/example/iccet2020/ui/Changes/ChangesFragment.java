3a029a5c8092a59c055ae6627f0ce18288cd751c
frishart500 <63844334+frishart500@users.noreply.github.com>
2020-12-02 00:11:13 +0200

    Артемий
    Заливай  это!

---
+++ app/src/main/java/com/example/iccet2020/ui/Changes/ChangesFragment.java
@@ -0,0 +1,26 @@
+package com.example.iccet2020.ui.Changes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.iccet2020.R;
+
+public class ChangesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+
+
+        return root;
+    }
+}
