6184d944783eb1270080a8b603dc42e7a5d7d94e
frishart500 <63844334+frishart500@users.noreply.github.com>
2020-12-02 00:56:37 +0200

    Артемий
    Принимайте работу Назар

--- app/src/main/java/com/example/iccet2020/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/iccet2020/ui/home/HomeFragment.java
@@ -10,10 +10,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.iccet2020.R;
+import com.example.iccet2020.ui.Changes.ChangesFragment;
+import com.google.android.gms.dynamic.FragmentWrapper;
 
 public class HomeFragment extends Fragment {
 
@@ -26,6 +30,22 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         changeBtn = root.findViewById(R.id.changeBtn);
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            Fragment fragment = null;
+            @Override
+            public void onClick(View view) {
+
+                    if(view.getId() == R.id.changeBtn){
+                        fragment = new ChangesFragment();
+                    }
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                    ft.replace(R.id.navigation_home, fragment);
+                    ft.commit();
+
+            }
+        });
+
 
         return root;
     }
